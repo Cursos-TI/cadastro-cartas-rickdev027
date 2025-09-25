87be0ff3345801054e38aade50c6bbcf0d48b886
HKDEV <rickgomes2707@gmail.com>
2025-09-25 04:05:16 +0000

    Definicao de variaveis para carta1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,23 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Vou Definir variaveis separadas para cada sistema de carta!!
+
+  //---VARIAVEIS DA CARTA 1 USAREI c1 PARA IDENTIFICAR TODAS VARIAVEIS DA CARTA 01-----//
+  // Estado: UMA LETRA A-H TIPO CHAR
+  char c1_estado; 
+  // Codigo da Cidade: SERA DECLARADO CODIGO DA CARTA EX: D01 USA UM  ARRAY DE CHAR COM TAMANHO 4 PARA QUANDO DER ENTER ELE ENTENDER O TERMINADOR \0
+  char c1_codigo[4];
+  // Nome da Cidade: UM ARRAY DE CHAR DE TAMANHO 50 PARA ARMAZENAR O NOME DAS CIDADES
+  char c1_nome_cidade[50];
+  // Populacao: NUMERO DE HABITANTES NA CIDADE Tipo INT
+  int c1_populacao;
+  // Area em km²: Tipo Float 
+  float c1_area_km2;
+  // PIB: EM BILHOES DE REAIS Tipo FLOAT
+  float c1_pib_bilhoes;
+  // Numero de Pontos Turisticos: Tipo INT
+  int c1_num_pontos_turisticos;
 
   // Área para entrada de dados
 
